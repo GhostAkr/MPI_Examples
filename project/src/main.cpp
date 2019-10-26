7f433531d14bb81711c1e8aaff89cd961df4f33a
Alexander Antoshkin <ighost99@gmail.com>
2019-10-26 19:02:40 +0300

    Inital commit.

---
+++ project/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "../include/mpiExample.h"
+
+using namespace std;
+
+int main() {
+	test();
+	system("pause");
+}
---
+++ project/src/mpiExample.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "../include/mpiExample.h"
+
+using namespace std;
+
+void test() {
+	cout << "Test" << endl;
+}
