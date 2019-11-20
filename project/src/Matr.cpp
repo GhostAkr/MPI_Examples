69efdb2c2a8c23f7ba55ccf654e60a9a79637324
Alexander Antoshkin <ighost99@gmail.com>
2019-11-20 16:35:28 +0300

    Usual matrix production was added.

--- project/src/Matr.cpp
+++ project/src/Matr.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+// Creation & destroying
+
 double** createMatr(int _rows, int _cols) {
 	double** result = new double*[_rows];
 	for (int i = 0; i < _rows; ++i) {
@@ -19,6 +21,28 @@ void deleteMatr(double** _matr, int _rows) {
 	}
 	delete[] _matr;
 }
+
+// Operations
+
+double** matrixMult(double** _source1, double** _source2, int m, int n, int s) {
+	double** result = new double*[m];
+	for (int i = 0; i < m; ++i) {
+		result[i] = new double[s];
+		for (int j = 0; j < s; ++j) {
+			result[i][j] = 0;
+		}
+	}
+	for (int k = 0; k < n; k++) {
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < s; j++) {
+				result[i][j] += _source1[i][k] * _source2[k][j];
+			}
+		}
+	}
+	return result;
+}
+
+// Staff things
 
 void printMatr(double** _mesh, int _rows, int _cols) {
 	for (int i = 0; i < _rows; ++i) {
--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 #include "../include/mpiProduction.h"
+#include "../include/Matr.h"
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
 	MPI_Init(&argc, &argv);
-	double t1 = MPI_Wtime();
-	cout << "Hello, world!" << endl;
-	double t2 = MPI_Wtime();
-	cout << "Time: " << t2 - t1 << endl;
+	double** m1 = new double* [2];
+	double** m2 = new double* [2];
+	for (int i = 0; i < 2; ++i) {
+		m1[i] = new double[2];
+		m2[i] = new double[2];
+	}
+	m1[0][0] = 1; m1[0][1] = 2; m1[1][0] = 3; m1[1][1] = 4;
+	m2[0][0] = 5; m2[0][1] = 6; m2[1][0] = 7; m2[1][1] = 8;
+	cout << "m1" << endl;
+	printMatr(m1, 2, 2);
+	cout << "m2" << endl;
+	printMatr(m2, 2, 2);
+	cout << "m1 * m2" << endl;
+	double** res = matrixMult(m1, m2, 2, 2, 2);
+	printMatr(res, 2, 2);
 	MPI_Finalize();
-	//system("pause");
+	system("pause");
 }
